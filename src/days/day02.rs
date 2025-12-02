bfd3e4160415467475fa571f85dd3535bae6d39c
Melwin Kramer <kramer@subshell.com>
2025-12-02 22:43:50 +0100

    Solution Day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -6,12 +6,58 @@ pub fn run() {
     println!("Day 02, Part 2: {}", solve_part2(&input));
 }
 
+type Range = (i64, i64);
+
+fn parse_ranges(input: &str) -> Vec<Range> {
+    input
+        .split(',')
+        .map(|r| {
+            let (start, end) = r.split_once('-').unwrap();
+            (start.parse().unwrap(), end.parse().unwrap())
+        })
+        .collect()
+}
+
 fn solve_part1(input: &str) -> i64 {
-    // Your solution here
-    0
+    let mut result = 0;
+    let ranges = parse_ranges(input);
+    
+    for range in ranges {
+        let (start, end) = range;
+        
+        for i in start..(end + 1) {
+            let number: String = i.to_string();
+            if number.len() % 2 != 0 {
+                continue;
+            }
+            let (first_half, second_half) = number.split_at(number.len() / 2);
+            if (first_half == second_half) {
+                result += i;
+            }
+        }
+    }
+    
+    result
 }
 
 fn solve_part2(input: &str) -> i64 {
-    // Your solution here
-    0
+    let mut result = 0;
+    let ranges = parse_ranges(input);
+    
+    for range in ranges {
+        let (start, end) = range;
+        for i in start..(end + 1) {
+            let number: String = i.to_string();
+            
+            for len in 1..(number.len() / 2 + 1) {
+                let replaced = number.replacen(number.split_at(len).0, "", number.len() / len);
+                if replaced.is_empty() {
+                    result += i;
+                    break;
+                }
+            }
+        }
+    }
+    
+    result
 }
